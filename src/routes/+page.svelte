d0d5b50ab734a6fd5823a3f7d1097fdbe210e44f
Dera <deraegbo@gmail.com>
2024-02-15 17:02:27 +0100

    Initialized project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,16 @@
 <script context="module">
 	import HudaText from "$lib/components/HudaText.svelte"
+	import { Avatar } from "@skeletonlabs/skeleton"
 </script>
 
-<section>
+<section class="rounded-container-token bg-blue-950 flex flex-col items-center justify-center mx-6 my-6 p-6">
+	<Avatar 
+		src="https://drive.google.com/thumbnail?id=1m78IXxOwWH3CImj5Y2DAzfOP3eBRCcur"
+		class="hover:!cursor-pointer p-1"
+		background="bg-white"
+	/>
 	<HudaText>The huda project</HudaText>
-	<HudaText style="font-size: 20px">Let's build something cool</HudaText>
+	<HudaText 
+		style="font-size: 20px">Let's build something cool
+	</HudaText>
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		padding-top: 20%;
-	}
-</style>
